cb9b59eb75f7961f39961bd3c0bdaacbc2cf925b
yoenotracompu <yoenotracompu@otracompu.com>
2022-04-25 13:18:58 -0300

    termine la practica 1-7

---
+++ variables.js
@@ -0,0 +1,34 @@
+//primer practica
+
+var anioEgresoFacu = 2012; //int
+
+var cantMascotasPoseo = 3; //int
+
+var millaAKm = 1.6; //float
+
+var medioLitro = 0.5; //float
+
+var hoyTrabajo = false //boolean
+
+var juevesCenso = false//boolean
+
+var miNombre = "natalia" //string
+
+var marcaDeBici = "olmo" //string
+
+//fin de primera parte
+
+// segunda practica
+
+document.write(`
+    <h4> año de egreso de la facultad: ${ anioEgresoFacu } </h4>
+    <h4> cantidad de mascotas que poseo: ${ cantMascotasPoseo } </h4>
+    <h4> milla a km: ${ millaAKm } </h4>
+    <h4> medio litro: ${ medioLitro } </h4>
+    <h4> hoy trabajo?: ${ hoyTrabajo} </h4>
+    <h4> el jueves es el censo?: ${ juevesCenso } </h4>
+    <h4> mi nombre es: ${ miNombre} </h4> 
+    <h4> una marca de bicicleta: ${ marcaDeBici } </h4>
+    
+
+`);
